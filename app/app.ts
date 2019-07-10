345bf0c7f5317c8b43f7a4b134323a6ef57b5456
Tomster <tomster@emberjs.com>
2019-07-10 07:38:11 -0400

    Initial Commit from Ember CLI v3.10.1
                                                            _...,
                                                         ,:^;,...;
                  -+===;.         ,,--++====++-,,,     .:  /.....,
                :::::~+++++#:,+#++++++++++++++++++#*..:   /,......
               (,,,,,,::=+++##++++++++++++++++++++++#.   :....../
                ...,,,,,::++++++++++++++++++++++++++++++*..,...:
                *..+...,#@@@@@@@@@++++++++++++++++++++++#*....*
                  @#,;##############@@@+*+#@@@@@@@@@@#*++#..<
                  *@##@@+,-*^^^*-+@####@@@######@@@#####@@,,,+
                    @#@*            @#@@@@#@@+--*^^*--#@@@@@@#
                    @#@.    @#      @##+++@#,           .@@#@@
                     #@#    @@     +@@++++#@@     @@     :@@
                     :@#*         @#@++++++@#*    #@     @@+
                    :*+@@#;,.__.+@#@+,-^^.++@#          @@++
                   ;*  :*@@@##@@@@;++r._j^.+@##@+,.__,,@@++.
                  /*    ........+++++++++++++#@@@@@###@@#++++,
                ,:       ...,@@@#++===----==@@@####,,....+++++
               .:       ......@@##@\   ;   :@####@,,...... +++.
               ;       .........@###,   ;  ;xx#@;,,.....   *;+,
               |       ........,*;xxxx--^--=xxx,........   :+#;
               ;         ......,,;xxxxxxxxxxxxx;,.....     *+#
                ;          ......,::xxxx;.     ......       +.   .
                 *;            .........  +###  ....       / ,. /:| ,.
                   .+:             ...  ;##++##, .      ,#. (..v..;*./
                      **                ##  ###*    .:*&&&+. \.,....<,
                       #&+**==-..,,__  ;##  ###  :,*+&&&&&&&v+#&,,.._/
                      #&&&&*...,::,,.  ##; ,##* .*****;:&&&&&&&&&
                     ,+*+;~*..*** *.* ### ###* *******    *+#&;*
                                      ##,;##    ****    :,  **
             #####    ##   ###  ###,  ########       .#####   ;##  ##
            #######  ;##  #### ,###.  ##########    ########  ### ####
           ###  ###  ### ##########   ####  ####   ,##   ###  #######*
          ### ,###  ##############:   ##     ###  #### ,##   :#### ###  ##;
      ##########    ########### ##   .##    ,###  #######    ##### :######
        ######    .###### ####  ##   ###    ### ######*    :#####   ####
           #############  ####  ################    ######## ###
            #####*  *#*    #:   :###   *###*         *####    #*

---
+++ app/app.ts
@@ -0,0 +1,48 @@
+import { setPropertyDidChange } from '@glimmer/tracking';
+import Application, { Resolver, ResolverDelegate } from 'glimmer-native';
+import { knownFolders } from 'tns-core-modules/file-system';
+
+const resolverDelegate = new ResolverDelegate();
+const resolver = new Resolver();
+let appFolder = knownFolders.currentApp();
+
+
+const addTemplates = (appFolder) => {
+  let templatesFile = appFolder.getFile('templates.json');
+  let templates = templatesFile.readTextSync();
+  // console.log(`Templates: ${templates}`);
+  JSON.parse(templates).forEach((template) => {
+    resolverDelegate.registerComponent(
+      template.name,
+      template.handle,
+      template.source,
+      template.capabilities
+    );
+  });
+};
+
+const addComponents = (appFolder) => {
+  let componentsFile = appFolder.getFile('components.json');
+  let components = componentsFile.readTextSync();
+  console.log(`About to resolve require`);
+  JSON.parse(components).forEach((component) => {
+    console.log(`About to resolve require`);
+    const classFile = require(`../src/ui/components/${component.name}/component.ts`);
+    resolver.registerComponent(component.name, classFile.default);
+  });
+};
+
+addTemplates(appFolder);
+addComponents(appFolder);
+
+const app = new Application('GlimmerTrackedIssue', resolverDelegate, resolver);
+// app.setup(appFolder);
+// const containerElement = document.getElementById('app');
+
+setPropertyDidChange(() => {
+  app.scheduleRerender();
+});
+
+// app.renderComponent('GlimmerTrackedIssue', app.rootFrame, null);
+
+app.boot();
---
+++ src/ui/components/GlimmerTrackedIssue/component.ts
@@ -0,0 +1,5 @@
+import Component from '@glimmer/component';
+
+export default class GlimmerTrackedIssue extends Component {
+    title = "Welcome to Glimmer Native"
+}
